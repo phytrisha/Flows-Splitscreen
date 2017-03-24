87078af45e27492cee668026685048cc655fa36c
phytrisha <markf.meyer@icloud.com>
2017-03-24 17:33:18 +0100

    -init

---
+++ js/colors.js
@@ -0,0 +1,9 @@
+// colors 
+
+var buttonStyleColors = [
+	'#4990E2',
+	'#7ED321',
+	'#F6A623',
+	'#D0011B',
+	'#50E3C2'
+]
---
+++ main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	// scale window elements
+	$('.leftView').css('height', window.innerHeight - 40 + 'px');
+	$('.rightView').css('height', window.innerHeight - 40 + 'px');
+	
+})
